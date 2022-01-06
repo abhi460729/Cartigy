64d79a68632516c4781559428a8c882fefceef91
unknown <abhi460729@gmail.com>
2022-01-06 09:03:01 +0530

    second commit
    Signed-off-by: unknown <abhi460729@gmail.com>

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express();
+const SERVER_PORT = process.env.PORT || 3333
 
 app.use(express.json())
 
@@ -10,7 +11,7 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/api',require('./routes/api').route)
 
-app.listen(2678, ()=>console.log('Server started at http://localhost:2678'))
+app.listen(SERVER_PORT, ()=>console.log('Server started at http://localhost:2678'))
 
 
 
